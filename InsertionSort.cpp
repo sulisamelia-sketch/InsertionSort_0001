bc4a849ee09d94044fdf9644a20ff583878e7fc5
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-12 14:54:20 +0700

    Menambahkan algoritma insertion sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -32,4 +32,21 @@ void input()    //procedure input
     }
 }
 
+void insertionsort()
+{       // procedure insertion sort
+    int temp;
+    int j;
 
+    for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        
+
+        
