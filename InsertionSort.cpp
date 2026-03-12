9f43cdf78d1f02f7aeb122abdc6243a9be3756b3
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-12 14:57:28 +0700

    Menambahkan procedure display

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -56,4 +56,18 @@ void insertionsort()
     }
 }
 
+void display()
+{
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElemen Array" << endl;
+    cout << "\n=================" ;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
 
