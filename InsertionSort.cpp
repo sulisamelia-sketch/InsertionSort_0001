5f1c5af42f345f9fde0626152ec6485e1c8c0d46
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-03-12 14:47:38 +0700

    Menambahkan procedure input jumlah data

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -4,3 +4,10 @@ using namespace std;
 int arr[20];        //membuat array dengan panjang data 20
 int n;             //membuat variable inputan n
 
+void input()    //procedure input
+{
+    while (true)
+    {
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+        
